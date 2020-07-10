98bee90fe4963785ac9c351e8027d3327617968d
caicailv <1401394657@qq.com>
2020-07-10 13:57:59 +0800

    修改border-1px问题解决方案

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,12 @@
 // postcss.config.js
 const autoprefixer = require('autoprefixer')
+const postCssBorder1px = require('./config/postcss-border-1px')
 module.exports = {
   plugins: [
     autoprefixer({
       overrideBrowserslist: ['Android 4.1', 'iOS 7.1'],
       grid: true,
     }),
+    postCssBorder1px
   ],
 }
