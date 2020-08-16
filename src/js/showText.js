ac274edf8d02a0f753d936650088a7ee79467e70
caicailv <1401394657@qq.com>
2020-08-16 18:57:50 +0800

    添加定制化配置,直接增加登录弹框,toast,修改jq版本

---
+++ src/js/showText.js
@@ -0,0 +1,82 @@
+import $ from 'jquery'
+var st_timer = null
+$(function () {
+  showInit()
+})
+
+function showInit() {
+  //透明黑色背景
+  var checkShowInit = true
+  $("<div id='st_mask' onclick='closeMask()'></div>").appendTo('body').css({
+    width: '100%',
+    height: '100%',
+    background: 'rgba(0,0,0,.4)',
+    position: 'fixed',
+    left: '0',
+    top: '0',
+    display: 'none',
+    'z-index': '1',
+  })
+
+  //---------------------------------在body最后添加Toast节点
+  $("<div id='st_toastBox'></div>").appendTo('body').css({
+    width: '100%',
+    position: 'fixed',
+    left: '0',
+    bottom: '10%',
+    'text-align': 'center',
+    display: 'none',
+  })
+  $("<span id='st_toastContent'></span>").appendTo('#st_toastBox').css({
+    color: '#fff',
+    background: 'rgba(0,0,0,.5 )',
+    padding: '10px 22px',
+    'border-radius': '20px',
+    'max-width': '80%',
+    display: 'inline-block',
+  })
+}
+function showToast(obj) {
+  if (!obj.text) {
+    return false
+  }
+  clearTimeout(st_timer)
+  $('#st_toastBox').hide()
+
+  var text = obj.text
+  var time = parseInt(obj.time ? obj.time : 2300)
+  var speed = obj.speed ? obj.speed : 'normal'
+  var bottom = obj.bottom ? obj.bottom : '10%'
+  if (obj.zindex) {
+    var zindex = parseInt(obj.zindex)
+    $('#st_mask').css({ 'z-index': zindex - 1 })
+    $('#st_toastBox').css({ 'z-index': zindex })
+  } else {
+    $('#st_mask').css({ 'z-index': 1 })
+    $('#st_toastBox').css({ 'z-index': 2 })
+  }
+
+  $('#st_toastBox').css({ bottom: bottom })
+
+  $('#st_toastContent').text(text)
+  $('#st_toastBox').fadeIn(speed)
+  st_timer = setTimeout(function () {
+    $('#st_toastBox').fadeOut()
+  }, time)
+}
+
+function closeMask() {
+  $('#st_mask,#st_alertBox,#st_confirmBox').hide()
+}
+function toast(msg) {
+  $(function () {
+    showToast({
+      text: msg,
+      bottom: '50%',
+      zindex: 99999,
+      speed: 500,
+      time: 1000,
+    })
+  })
+}
+export default toast
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import './js/flexible'  //屏幕适配
-import './css/reset.css';//基础样式
-import '@/css/index.scss';    //页面样式
-// import $ from 'jquery';       //使用插件  
-// window.$ = $;
-
-
-
+import './js/flexible' //屏幕适配
+import './css/reset.css' //基础样式
+import '@/css/index.scss' //页面样式
+import '@/css/popup.scss' //弹窗样式,可选 内置登录弹窗
+import $ from './js/jquery' //使用插件
+import toast from './js/showText' //使用插件
+window.$ = $
+window.toast = toast
