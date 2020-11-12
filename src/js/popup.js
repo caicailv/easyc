aad4fbd7f3333a883e6c431e368cff098fb9ceb0
caicailv <1401394657@qq.com>
2020-11-12 17:10:46 +0800

    a

---
+++ src/js/popup.js
@@ -0,0 +1,83 @@
+import $ from './jquery' //使用插件
+
+/* 
+            popup
+            title: 是否展示标题
+            content: 内容
+            confirmText: 确认按钮文字 如果传空表示不展示确认按钮
+            confirmCallback: 点击确认回调,函数内this是该弹窗dom对象
+            offCallback: 关闭回调,函数内this是该弹窗dom对象
+            tips: 提示语文字
+            conLeft: //展示左按钮, 文字是按钮文案
+            conRight: //展示左按钮, 文字是按钮文案
+            conLeftCallback 左按钮点击回调
+            conRightCallback 右按钮点击回调
+            rule 规则文案
+*/
+
+let popupId = 0
+export default function popup({
+  title = '',
+  content = '',
+  confirmText = '我知道了',
+  confirmCallback = function () {},
+  offCallback = function () {},
+  offShow = false,
+  tips = '',
+  rule = '',
+  conLeft = '',
+  conRight = '',
+  conLeftCallback = function () {},
+  conRightCallback = function () {},
+}) {
+  popupId++
+  let html = `
+  <div class="popup show" id="popup_${popupId}">
+    <div class="popup_row  ${rule ? 'rulerow' : ''} ${title ? 'tit' : ''}">
+        ${offShow ? '<div class="pop_off"></div>' : ''}
+        ${title ? '<div class="pop_title"></div>' : ''}
+        ${content ? `<div class="content">${content}</div>` : ''}
+        ${rule ? `<div class="rule">${rule}</div>` : ''}
+        ${
+          conLeft && conRight
+            ? `<div class="fot_rot">
+            <div class="fot_left">${conLeft}</div>
+            <div class="fot_right">${conRight}</div>
+        </div>`
+            : ''
+        }
+        ${confirmText!=="" ? `<div class="fot_btn">${confirmText}</div>` : ''}
+        ${tips ? `<div class="p_tips">${tips}</div>` : ''}
+      </div>
+    </div>
+  `
+  $('body').append(html)
+  const popElement = $('#popup_' + popupId)[0]
+  $(popElement)
+    .find('.fot_btn')
+    .click(function () {
+      confirmCallback.call(popElement)
+    })
+  $(popElement)
+    .find('.pop_off')
+    .click(function () {
+      offCallback.call(popElement)
+      $(popElement).remove()
+    })
+  $(popElement)
+    .find('.pop_off2')
+    .click(function () {
+      offCallback.call(popElement)
+      $(popElement).remove()
+    })
+  $(popElement)
+    .find('.fot_left')
+    .click(function () {
+      conLeftCallback.call(popElement)
+    })
+  $(popElement)
+    .find('.fot_right')
+    .click(function () {
+      conRightCallback.call(popElement)
+    })
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import './js/flexible' //屏幕适配
 import './css/reset.css' //基础样式
 import '@/css/index.scss' //页面样式
-import '@/css/popup.scss' //弹窗样式,可选 内置登录弹窗
+import '@/css/popup.scss' //弹窗样式
 import $ from './js/jquery' //使用插件
-import toast from './js/showText' //使用插件
+import popup from './js/popup'
+import toast from './js/showText'
+import VConsole from 'vConsole'
 window.$ = $
 window.toast = toast
+window.popup = popup
+
+// 如果需要移动端调试
+var vConsole = new VConsole() 
