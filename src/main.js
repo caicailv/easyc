04ab207a94aa7f6efd4b8b5979827522d8b4a967
caicailv <1401394657@qq.com>
2020-11-12 18:05:27 +0800

    增加规则页面

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './js/flexible' //屏幕适配
 import './css/reset.css' //基础样式
 import '@/css/index.scss' //页面样式
+import '@/css/rule.scss' //页面样式
 import '@/css/popup.scss' //弹窗样式
 import $ from './js/jquery' //使用插件
 import popup from './js/popup'
@@ -11,4 +12,4 @@ window.toast = toast
 window.popup = popup
 
 // 如果需要移动端调试
-var vConsole = new VConsole() 
+// var vConsole = new VConsole() 
